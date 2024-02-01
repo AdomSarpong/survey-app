e06706971c37202b3796476b1746aca1457b0a9b
adom <xsunsetxdreamx@gmail.com>
2024-02-01 15:59:47 -0500

    Updated PROD URL in quasar.config.js

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -62,7 +62,7 @@ module.exports = configure(function ( ctx ) {
 
       vueRouterMode: 'history', // available values: 'hash', 'history'
       env: {
-        BASE_URL: ctx.dev ? 'http://localhost:3000' : 'http://<18.212.4.53>',
+        BASE_URL: ctx.dev ? 'http://localhost:3000' : 'http://<3.91.149.154>',
         CREATOR_NAME: 'Adom Sarpong',
         CREATOR_EMAIL: 'xsunsetxdreamx@gmail.com',
         LINKEDIN: 'https://www.linkedin.com/in/adom-sarpong-1607052b0/'
